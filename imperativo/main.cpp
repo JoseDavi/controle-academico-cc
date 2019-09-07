532b102d03a0dfa732fb5df922e86c9496e0de02
Wesley-M <wesley.santos@ccc.ufcg.edu.br>
2019-09-07 07:51:49 -0300

    Modificado uso de arrays em usuários para uso de vector e mudança de convenção de nome de funções para facilitar a leitura

--- imperativo/main.cpp
+++ imperativo/main.cpp
@@ -3,28 +3,31 @@
 #include <stdlib.h>
 #include <string>
 #include <map>
+#include <vector>
 
 using namespace std;
 
 enum estado {matricula = 1, emcurso = 2, fimdeperiodo = 3};
-void mainmenu();
-void menuinicial();
-void menualuno();
-void menuprofessor();
-void menucoordenador();
-bool validausuario(string username, string password);
-void menulogin();
-void atualizatipodeusuario(string username);
 
+// Menus dos funcionários
+void main_menu();
+void menu_inicial();
+void menu_aluno();
+void menu_professor();
+void menu_coordenador();
+void menu_login();
+
+bool valida_usuario(string username, string password);
+void atualiza_tipo_de_usuario(string username);
 
 int main() {
     // TODO  Implementar o código que carrega todos os dados cadastrados no CSV;
     // a ideia é de otimizar a permanencia de dados, mas inves de manipular arquivos diretamente(processo
     // altamente custoso), fazemos isso apenas duas vezes: no início e final da execução do programa.
-    
+
     // TODO issue #5 (carregar disciplinas)
 
-    mainmenu();
+    main_menu();
 
     // TODO  Implementar o código que armazena/atualiza os dados da estrutura de dados no arquivo CSV;
     return 0;
@@ -33,59 +36,54 @@ int main() {
 int usrtipo = 0; // alterar para 0 depois
 string username;
 string password;
+
 //Users é um array de arrays, onde os arrays mais internos tem 3 posições, nome, senha e tipo, mas o plano é
 //guardar também matricula, e talvez um array com as cadeiras que ele já pagou, ou um apontador pra elas.
 //Inicie ele com poucas posições para testar, mas a gente pode aumentar ou diminur se necessário.
 //Lembrando que estou utilizando username como identificador, talvez seja melhor identificar o user por outro meio.
-string users[100][3];
+
+/* Penso que seja melhor usarmos um vector (array dinâmico) para guardar os usuários,
+   sendo mais eficiente para memória e retirando limites de tamanho de arrays */
+vector <array<string, 3>> users = {
+  {"joao", "123", "coordenador"},
+  {"jose", "321", "professor"},
+  {"jonas", "456", "aluno"}
+};
+
 int command = 0;
-void mainmenu() {
-//Coloquei aqui dentro do metodo de validação somente para testar a implementação, não pensei num jeito
-//de puxar os users lá do main sem ficar sempre mandando o array, o que achei que não ia ficar muito legal.
 
-// user teste 1, joao com senha 123, coordenador.
-users[0][0] = "joao";
-users[0][1] = "123";
-users[0][2] = "coordenador";
-// user teste 2, jose com senha 321, professor.
-users[1][0] = "jose";
-users[1][1] = "321";
-users[1][2] = "professor";
-//user teste 3, jonas com senha 456, aluno.
-users[2][0] = "jonas";
-users[2][1] = "456";
-users[2][2] = "aluno";
+void main_menu() {
 
-    menuinicial();
+    menu_inicial();
     while (true) {
         if(command == 0){
-        menuinicial();
+          menu_inicial();
         }
 
         if (command == 2) {
             printf("quit...\n");
             exit(0);
-        } 
-        
+        }
+
         if (command == 1) {
-            menulogin();
+            menu_login();
             if (usrtipo == 1) {
-                menualuno();
+                menu_aluno();
             } else if (usrtipo == 2) {
-                menuprofessor();
+                menu_professor();
             } else if (usrtipo == 3) {
-                menucoordenador();
+                menu_coordenador();
             } else {
                 // command = 0; // vai para tela inicial...
             }
         }
         fflush(stdin);
-        command = 0; 
+        command = 0;
     }
 }
 
 
-void menuinicial() {
+void menu_inicial() {
     printf("\n| ------------ Controle Academico CC ------------ |\n\n");
     printf("1) Entrar \n");
     printf("2) Sair\n");
@@ -94,7 +92,7 @@ void menuinicial() {
 }
 
 
-void menulogin() {
+void menu_login() {
 
     if (!username.empty() && !password.empty()){
         return; // o usuário já esta logado.
@@ -110,17 +108,17 @@ void menulogin() {
     cin >> psw;
     password = psw;
 
-    if (!validausuario(username,password)) {
+    if (!valida_usuario(username,password)) {
         username = "";
         password = "";
         return;
     }
-    atualizatipodeusuario(username);
+    atualiza_tipo_de_usuario(username);
 
 }
 
 //Bem simples, só checando se o usuario existe no array de usuarios, e se ele existir, checando se a senha corresponde.
-bool validausuario(string username, string password) {
+bool valida_usuario(string username, string password) {
     for(int i = 0; i < 100;i++){
         if(users[i][0].compare(username)==0){
             if(users[i][1].compare(password)==0){
@@ -128,14 +126,14 @@ bool validausuario(string username, string password) {
             }else{
                 cout << "Senha inválida.";
                 return false;
-        }   
+            }
         }
     }
     return false;
 }
 
 //Bem simplificado, somente a checagem do tipo em string e atribuindo o usrtipo ao numero equivalente.
-void atualizatipodeusuario(string username) {
+void atualiza_tipo_de_usuario(string username) {
      for(int i = 0; i < 100;i++){
         if(users[i][0].compare(username)==0){
             if(users[i][2].compare("aluno")==0){
@@ -153,7 +151,7 @@ void atualizatipodeusuario(string username) {
 }
 
 
-void menualuno() {
+void menu_aluno() {
 
     if (username.empty() || password.empty()) {
         printf("INVALID USER !");
@@ -185,8 +183,8 @@ void menualuno() {
 }
 
 
-void menuprofessor() {
-    
+void menu_professor() {
+
     if (username.empty() || password.empty()) {
         printf("INVALID USER !");
         return;
@@ -214,8 +212,8 @@ void menuprofessor() {
 }
 
 
-void menucoordenador() {
-    
+void menu_coordenador() {
+
     if (username.empty() || password.empty()) {
         printf("INVALID USER !");
         return;
