f3de3b05121dc595da8a87ca7ed567ae354b63f8
Wesley-M <wesley.santos@ccc.ufcg.edu.br>
2019-10-03 14:22:54 -0300

    Criando arquivos json para representar alunos e usuarios, alem disso foram implementadas funçoes de leitura para esses arquivos, que ja estao funcionando

--- funcional/Persistence.hs
+++ funcional/Persistence.hs
@@ -4,19 +4,47 @@
 module Persistence where
 
 import qualified Data.Text as T
+import qualified Data.ByteString.Lazy as B
+
 import GHC.Generics
 import Data.Aeson
+import Data.Maybe
 
 -- Representação do usuário no sistema
 data Usuario = Usuario
     { matricula :: String
     , senha :: String
-    , tipo :: String
+    , tipoUsuario :: String
     , nomeUsuario :: String
-    } deriving (Show,Generic)
+    } deriving (Show, Generic, Eq)
 
 instance FromJSON Usuario
 instance ToJSON Usuario
+
+
+-- Representação do aluno no sistema
+data Aluno = Aluno
+    { matriculaAluno :: String
+    , nomeAluno :: String
+    , disciplinasMatriculadas :: Int
+    , estaDesvinculado :: Bool
+    , disciplinas :: [MetaDisciplina]
+    } deriving (Show, Generic, Eq)
+
+instance FromJSON Aluno
+instance ToJSON Aluno
+
+-- Representação de disciplina de um aluno
+data MetaDisciplina = MetaDisciplina
+    { idMetaDisciplina :: String
+    , nomeMetaDisciplina :: String
+    , faltas :: Int
+    , notas :: [Double]
+    , estado :: String
+    } deriving (Show, Generic, Eq)
+
+instance FromJSON MetaDisciplina
+instance ToJSON MetaDisciplina
 
 -- Representação da disciplina no sistema
 data Disciplina = Disciplina
@@ -25,7 +53,33 @@ data Disciplina = Disciplina
     , limite :: Int
     , p_requisito :: Int
     , s_requisito :: Int
-    } deriving (Show, Generic)
+    } deriving (Show, Generic, Eq)
 
 instance FromJSON Disciplina
 instance ToJSON Disciplina
+
+
+{- Seção para recuperação de informações dos arquivos -}
+
+leDisciplinas :: IO ([Disciplina])
+leDisciplinas = do
+    byteStrDisciplinas <- B.readFile "resources/disciplinas.json"
+    let disciplinas = fromJust (decode byteStrDisciplinas :: Maybe [Disciplina])
+    return (disciplinas)
+
+leUsuarios :: IO ([Usuario])
+leUsuarios = do
+    byteStrUsuarios <- B.readFile "resources/usuarios.json"
+    let usuarios = fromJust (decode byteStrUsuarios :: Maybe [Usuario])
+    return (usuarios)
+
+leAlunos :: IO ([Aluno])
+leAlunos = do
+    byteStrAlunos <- B.readFile "resources/alunos.json"
+    let alunos = fromJust (decode byteStrAlunos :: Maybe [Aluno])
+    return (alunos)
+
+removeAluno :: Aluno -> [Aluno] -> [Aluno]
+removeAluno _ []  = []
+removeAluno a (a1:an) | a == a1 = removeAluno a an
+                      | otherwise = a1 : removeAluno a an
